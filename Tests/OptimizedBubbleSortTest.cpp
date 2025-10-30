022101d1ebb6b95217cfa9a1bf8c6dcaaf8ca569
tarvld <2506.taran@gmail.com>
2025-10-30 10:58:55 +0300

    Доработка

--- Tests/OptimizedBubbleSortTest.cpp
+++ Tests/OptimizedBubbleSortTest.cpp
@@ -1,34 +1,34 @@
 //#define CATCH_CONFIG_MAIN
-#include "C:\Users\User\source\repos\Структруры\SortingTests\Tests\catch.hpp"
+#include "C:\Users\User\source\repos\Structs\SortingTests\Tests\catch.hpp"
 #include <algorithm>
 #include "../Sorts/OptimizedBubbleSort.h"
 #include "../Sorts/Func.h"
 
-TEST_CASE("OptimizedBubble Проверка нулевого массива", "[optimized][unit]") {
+TEST_CASE("OptimizedBubble РџСЂРѕРІРµСЂРєР° РЅСѓР»РµРІРѕРіРѕ РјР°СЃСЃРёРІР°", "[optimized][unit]") {
 	int arr[3] = {};
 	int ShouldBeArr[3] = {};
 	OptimizedBubbleSort(arr, 3);
 	REQUIRE(arrEqual(arr, ShouldBeArr, 3));
 }
-TEST_CASE("OptimizedBubble Проверка массива с одним элементом", "[optimized][unit]") {
+TEST_CASE("OptimizedBubble РџСЂРѕРІРµСЂРєР° РјР°СЃСЃРёРІР° СЃ РѕРґРЅРёРј СЌР»РµРјРµРЅС‚РѕРј", "[optimized][unit]") {
 	int arr[1] = { 5 };
 	int ShouldBeArr[1] = { 5 };
 	OptimizedBubbleSort(arr, 1);
 	REQUIRE(arrEqual(arr, ShouldBeArr, 1));
 }
-TEST_CASE("OptimizedBubble Проверка  отсортированного массива", "[optimized][unit]") {
+TEST_CASE("OptimizedBubble РџСЂРѕРІРµСЂРєР°  РѕС‚СЃРѕСЂС‚РёСЂРѕРІР°РЅРЅРѕРіРѕ РјР°СЃСЃРёРІР°", "[optimized][unit]") {
 	int arr[5] = { 1, 2, 3, 4, 5 };
 	int ShouldBeArr[5] = { 1, 2, 3, 4, 5 };
 	OptimizedBubbleSort(arr, 5);
 	REQUIRE(arrEqual(arr, ShouldBeArr, 5));
 }
-TEST_CASE("OptimizedBubble Проверка обратного массива", "[optimized][unit]") {
+TEST_CASE("OptimizedBubble РџСЂРѕРІРµСЂРєР° РѕР±СЂР°С‚РЅРѕРіРѕ РјР°СЃСЃРёРІР°", "[optimized][unit]") {
 	int arr[5] = { 5, 4, 3, 2, 1 };
 	int ShouldBeArr[5] = { 1, 2, 3, 4, 5 };
 	OptimizedBubbleSort(arr, 5);
 	REQUIRE(arrEqual(arr, ShouldBeArr, 5));
 }
-TEST_CASE("OptimizedBubble Проверка массива double", "[optimized][unit]") {
+TEST_CASE("OptimizedBubble РџСЂРѕРІРµСЂРєР° РјР°СЃСЃРёРІР° double", "[optimized][unit]") {
 	double arr[5] = { 5.5, 4.4, 3.3, 2.2, 1.1 };
 	double ShouldBeArr[5] = { 1.1, 2.2, 3.3, 4.4, 5.5 };
 	OptimizedBubbleSort(arr, 5);
--- Tests/SelectionSortTest.cpp
+++ Tests/SelectionSortTest.cpp
@@ -1,35 +1,35 @@
 //#define CATCH_CONFIG_MAIN
-#include "C:\Users\User\source\repos\Структруры\SortingTests\Tests\catch.hpp"
+#include "C:\Users\User\source\repos\Structs\SortingTests\Tests\catch.hpp"
 #include <algorithm>
 #include "../Sorts/SelectionSort.h"
 #include "../Sorts/Func.h"
 
 
-TEST_CASE("Selection Проверка нулевого массива", "[bubble][unit]") {
+TEST_CASE("Selection РџСЂРѕРІРµСЂРєР° РЅСѓР»РµРІРѕРіРѕ РјР°СЃСЃРёРІР°", "[bubble][unit]") {
 	int arr[3] = {};
 	int ShouldBeArr[3] = {};
 	SelectionSort(arr, 3);
 	REQUIRE(arrEqual(arr, ShouldBeArr, 3));
 }
-TEST_CASE("Selection Проверка массива с одним элементом", "[bubble][unit]") {
+TEST_CASE("Selection РџСЂРѕРІРµСЂРєР° РјР°СЃСЃРёРІР° СЃ РѕРґРЅРёРј СЌР»РµРјРµРЅС‚РѕРј", "[bubble][unit]") {
 	int arr[1] = { 5 };
 	int ShouldBeArr[1] = { 5 };
 	SelectionSort(arr, 1);
 	REQUIRE(arrEqual(arr, ShouldBeArr, 1));
 }
-TEST_CASE("Selection Проверка  отсортированного массива", "[bubble][unit]") {
+TEST_CASE("Selection РџСЂРѕРІРµСЂРєР°  РѕС‚СЃРѕСЂС‚РёСЂРѕРІР°РЅРЅРѕРіРѕ РјР°СЃСЃРёРІР°", "[bubble][unit]") {
 	int arr[5] = { 1, 2, 3, 4, 5 };
 	int ShouldBeArr[5] = { 1, 2, 3, 4, 5 };
 	SelectionSort(arr, 5);
 	REQUIRE(arrEqual(arr, ShouldBeArr, 5));
 }
-TEST_CASE("Selection Проверка обратного массива", "[bubble][unit]") {
+TEST_CASE("Selection РџСЂРѕРІРµСЂРєР° РѕР±СЂР°С‚РЅРѕРіРѕ РјР°СЃСЃРёРІР°", "[bubble][unit]") {
 	int arr[5] = { 5, 4, 3, 2, 1 };
 	int ShouldBeArr[5] = { 1, 2, 3, 4, 5 };
 	SelectionSort(arr, 5);
 	REQUIRE(arrEqual(arr, ShouldBeArr, 5));
 }
-TEST_CASE("Selection Проверка массива double", "[bubble][unit]") {
+TEST_CASE("Selection РџСЂРѕРІРµСЂРєР° РјР°СЃСЃРёРІР° double", "[bubble][unit]") {
 	double arr[5] = { 5.5, 4.4, 3.3, 2.2, 1.1 };
 	double ShouldBeArr[5] = { 1.1, 2.2, 3.3, 4.4, 5.5 };
 	SelectionSort(arr, 5);
